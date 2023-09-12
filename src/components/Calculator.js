9fe3447172fd8400eec16287aca6598d534c9da2
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-12 10:50:02 +0300

    Add calculator input buttons

--- src/components/Calculator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Calculator = () => (
-  <div>
-    <h1>Hello, Calculator</h1>
-  </div>
-);
-
-export default Calculator;
---
+++ src/components/calculator/CalculatorOutput.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CalculatorOutput = () => (
+  <div className="output" id="output">0</div>
+);
+
+export default CalculatorOutput;
