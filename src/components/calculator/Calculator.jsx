d84edd539cc6ea091469a032c236ef7368656ad0
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-21 14:37:46 +0300

    UI/UX improvements

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -17,9 +17,15 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <CalculatorOutput calculatorData={calculatorData} />
-      <CalculatorInputs handleClick={handleClick} />
+    <div className="calculator-div">
+      <div>
+        <h2>Let&apos;s do some math!</h2>
+      </div>
+      <div className="calculator">
+        <CalculatorOutput calculatorData={calculatorData} />
+        <CalculatorInputs handleClick={handleClick} />
+      </div>
+
     </div>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,10 +4,10 @@ import styles from '../styles/Header.module.css';
 const Header = () => (
   <nav className={styles.nav}>
     <h1>Math Magicians</h1>
-    <ul className={styles.ul}>
-      <li><a href="/">Home</a></li>
-      <li><a href="/calculator">Calculator</a></li>
-      <li><a href="/quote">Quote</a></li>
+    <ul>
+      <a href="/"><li>Home</li></a>
+      <a href="/calculator"><li>Calculator</li></a>
+      <a href="/quote"><li>Quote</li></a>
     </ul>
   </nav>
 );
