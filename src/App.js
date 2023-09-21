464734bc835f1ce8674c259966d3d578e3b6253f
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-21 10:20:27 +0300

    Add react-routing and home,header components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { BrowserRoute, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Homepage';
+import Header from './components/layout/Header';
 import Calculator from './components/calculator/Calculator';
 import Quotes from './components/quotes/showQuotes';
 import './App.css';
@@ -5,8 +8,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <Quotes />
+      <Header />
+      <BrowserRoute>
+        <Routes>
+          <Route>
+            <Home />
+          </Route>
+          <Route>
+            <Calculator />
+          </Route>
+          <Route>
+            <Quotes />
+          </Route>
+        </Routes>
+      </BrowserRoute>
     </div>
   );
 }
